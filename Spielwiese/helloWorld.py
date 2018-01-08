66afd8e7fa90c83f9f36e673e2d3685447c8e8b6
Michael Borko <michael.borko@tgm.ac.at>
2018-01-08 09:12:29 +0100

    Kurze Intro zu Python

---
+++ Spielwiese/helloWorld.py
@@ -0,0 +1,5 @@
+
+print("Hello World!")
+name = input("Sag mir bitte deinen Namen: ")
+#print("Hallo %s, ist heute nicht ein wunderschöner Tag!?" % name)
+print("Hallo "+name+", ist heute nicht ein wunderschöner Tag!?")
