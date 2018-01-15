096ca7d33e06490adbfe20dad24e8bfb1194538e
Michael Borko <michael.borko@tgm.ac.at>
2018-01-15 16:07:43 +0100

    status quo

--- sew4_threading-simple-encryption/src/main/python/encryption/crypt.py
+++ sew4_threading-simple-encryption/src/main/python/encryption/crypt.py
@@ -33,15 +33,50 @@ class Cryptography():
         self.__threadcount = threads
 
     def getEncryptedMessage(self):
-        pass
+        SimpleEncrypter.setMessage(self.__message)
+
+        threads = []
+        for i in range(0, self.__threadcount):
+            thread = SimpleEncrypter(i, self.__threadcount)
+            thread.start()
+            threads.append(thread)
+
+        for t in threads:
+            t.join()
+
+        return SimpleEncrypter.getMessage()
 
     def getDecryptedMessage(self):
-        pass
+        SimpleEncrypter.setDecryption(True)
+        threads = []
+        for i in range(0, self.__threadcount):
+            thread = SimpleEncrypter(i, self.__threadcount)
+            thread.start()
+            threads.append(thread)
+
+        for t in threads:
+            t.join()
+
+        return SimpleEncrypter.getMessage()
+
 
 def main():
+    message = input("Give me please the message for encryption: ")
+    threads = input("How many threads do you want to start? ")
+    dictionary = {' ': ' ', 'A': 'V', 'B': 'J', 'C': 'Z', 'D': 'B', 'E': 'G', 'F': 'N', 'G': 'F',
+                                'H': 'E',
+                                'I': 'P', 'J': 'L', 'K': 'I', 'L': 'T', 'M': 'M', 'N': 'X', 'O': 'D', 'P': 'W',
+                                'Q': 'K',
+                                'R': 'Q', 'S': 'U', 'T': 'C', 'U': 'R', 'V': 'Y', 'W': 'A', 'X': 'H', 'Y': 'S',
+                                'Z': 'O'}
+    crypt = Cryptography(message, dictionary, threads)
+    encryptedMessage = crypt.getEncryptedMessage()
+    print("Your encrypted message: " + encryptedMessage )
+
     decryptedMessage = crypt.getDecryptedMessage()
     print('Your decrypted message: %s' % decryptedMessage)
 
-#if  __name__ == '__main__':
-#    main()
+
+if  __name__ == '__main__':
+    main()
 
--- sew4_threading-simple-encryption/src/main/python/encryption/simpleencrypter.py
+++ sew4_threading-simple-encryption/src/main/python/encryption/simpleencrypter.py
@@ -5,32 +5,51 @@ Created on 02.11.2017
 @version: 20171102
 
 @description: Implementation eines einfachen Encryptors für A bis Z,
-              aber auch zusätzlich für alle visible ascii chars in einem erweiterten Schritt
+              aber auch zusätzlich für alle visible ascii chars
+              in einem erweiterten Schritt
 """
 import threading
+import operator
 
 
 class SimpleEncrypter(threading.Thread):
 
+    message = []
+    dictionary = dict()
+    lock = threading.Lock()
+    decryption = False
+
     def __init__(self, firstIndex, offset):
-        pass
+        threading.Thread.__init__(self)
+        self.__firstIndex = firstIndex
+        self.__offset = offset
 
     @staticmethod
-    def setCryptography(dic1, distance=0, decryption=False): #changed parameter name dict to dic1 since dict is a reserved word
-        pass
+    def setCryptography(dictionary, distance=0, decryption=False): #changed parameter name dict to dic1 since dict is a reserved word
+        SimpleEncrypter.dictionary = dictionary
+        SimpleEncrypter.setDecryption(decryption)
 
     @staticmethod
     def setDecryption(enabled):
-        pass
-
+        if operator.xor(SimpleEncrypter.decryption, enabled):
+            # https://stackoverflow.com/questions/7360757/reversing-dictionary-in-python
+            dict(map(reversed, SimpleEncrypter.dictionary.items()))
+            SimpleEncrypter.decryption = not SimpleEncrypter.decryption
+        print(SimpleEncrypter.dictionary)
+cd
     @staticmethod
     def setMessage(message):
-        pass
+        SimpleEncrypter.message = list(message)
 
     @staticmethod
     def getMessage():
-        pass
+        return ''.join(SimpleEncrypter.message)
 
     def run(self):
-        pass
-
+        # en/decrypt message through dictionary mapping
+        i = self.__firstIndex
+        while i < len(SimpleEncrypter.message):
+            with SimpleEncrypter.lock:
+                SimpleEncrypter.message[i] = SimpleEncrypter.dictionary[SimpleEncrypter.message[i]]
+            # TODO not sure if we have do add 1
+            i += self.__offset
